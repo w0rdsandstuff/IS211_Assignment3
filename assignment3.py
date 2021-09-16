5fc4e096a27cbe6be0b15a729619169bcd871a88
w0rdsandstuff <77218212+w0rdsandstuff@users.noreply.github.com>
2021-09-16 15:20:08 -0400

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,6 +1,51 @@
 import argparse
-# other imports go here
+import urllib.request
+import csv
+import re
 
+def downloadData(url):
+    request = urllib.request.urlopen(url)
+    data = request.read().decode('utf-8')
+    return data
+
+def processImages(data):
+    lines = 0
+    jpg = 0
+    gif = 0
+    png = 0
+
+    for row in data:
+        lines = lines + 1
+        if re.search(r".jpg", row[0], re.IGNORECASE):
+            jpg += 1
+
+        elif re.search(r".gif", row[0], re.IGNORECASE):
+            gif += 1
+
+        elif re.search(r".png", row[0], re.IGNORECASE):
+            png += 1
+
+            
+    return jpg + gif + png
+    print('Image requests account for {(jpg+gif+png/lines)*100} of all requests')
+    
+
+def processBrowsers(data):
+    ff = 0
+    chrome = 0
+    ie = 0
+    safari = 0
+    for row in data:
+        if re.search(r"Firefox", row[2], re.IGNORECASE):
+            ff += 1
+        if re.search(r"Chrome", row[2], re.IGNORECASE):
+            chrome += 1
+        if re.search(r"Trident", row[2], re.IGNORECASE):
+            ie += 1
+        if re.search(r"AppleWebkit", row[2], re.IGNORECASE):
+            safari += 1
+
+    return max(ff, chrome, ie, safari)
 
 def main(url):
     print(f"Running main with URL = {url}...")
